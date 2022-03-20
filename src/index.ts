352c34d1a98270eb34e1720072fa9e23d6c80c61
Daniele Agosti <sddania@gmail.com>
2022-03-20 23:17:54 +0100

    manage effects with IO

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 import yargs from "yargs/yargs";
 import { parseFileAndGetResults } from "./services/parse-file-and-get-results";
+import { pipe } from "fp-ts/function";
+import * as Console from "fp-ts/Console";
+import * as E from "fp-ts/Either";
+import * as IO from "fp-ts/IO";
+import * as A from "fp-ts/Array";
 
 const parser = yargs(process.argv.slice(2)).options({
   path: { type: "string", demand: true }
@@ -8,14 +13,13 @@ const parser = yargs(process.argv.slice(2)).options({
 
 (async () => {
   const argv = await parser.argv;
-  const results = parseFileAndGetResults(argv.path);
-  // todo: manage side effects
-  switch (results._tag) {
-    case "Left":
-      console.log(results.left, "color: red;");
-      break;
-    case "Right":
-      results.right.forEach(r => console.log(r));
-      break;
-  }
+  const printResults = pipe(
+    argv.path,
+    parseFileAndGetResults,
+    E.fold(
+      Console.error,
+      r => IO.of(A.map(Console.info)([...r]))
+    ))
+
+  printResults()
 })();
